2a3a16775dedd9ea58d2dcec9b855885d9ae631b
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-01-30 18:55:45 -0300

    Update: use the app directory.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  },
+  images: {
+    domains: ["http2.mlstatic.com"]
+  },
   reactStrictMode: true,
 }
 
