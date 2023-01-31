9902876ac6ff217abe9eedb75f61a519dc130418
Pablo Peña <93575694+sebas-pena@users.noreply.github.com>
2023-01-30 23:05:38 -0300

    Update: show powered by message.

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -4,14 +4,24 @@ import React from "react"
 
 const Footer = () => {
 	return (
-		<footer className="text-center">
-			<Link
-				href="https://github.com/sebas-pena/"
-				className="inline-flex justify-center items-center gap-2 py-4 font-semibold"
-			>
-				<Image width={18} height={18} alt="github icon" src="/svg/github.svg" />
-				<span>sebas-pena</span>
-			</Link>
+		<footer className="text-center py-4">
+			<div className="inline-flex justify-center items-center font-semibold">
+				<p>
+					<Link href="https://github.com/sebas-pena/">
+						<span>sebas-pena</span>
+					</Link>
+					<span>/</span>
+					<Link href="https://github.com/sebas-pena/Astro-Supporter">
+						astro-supporter
+					</Link>
+				</p>
+			</div>
+			<p>
+				powered by{" "}
+				<Link href="https://cohere.ai/" className="font-semibold">
+					Cohere ai
+				</Link>
+			</p>
 		</footer>
 	)
 }
